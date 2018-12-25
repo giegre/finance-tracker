3d9e1dca1901ea5b4eb7b50ab934bb33938603c5
Fachri Akbar Rahmanda <g.ficheron@gmail.com>
2018-12-25 11:29:46 +0700

    add friends profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def my_portfolio
     @user = current_user
     @user_stocks = current_user.stocks
@@ -19,6 +20,23 @@ class UsersController < ApplicationController
     respond_to do | format |
       format.js { render partial: 'friends/result' }
     end
+  end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+
+    if current_user.save
+      flash[:success] = 'Friend was successfully added'
+    else
+      flash[:danger] = 'There was something wrong with the friend request'
+    end
+    redirect_to my_friends_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks.all
   end
 
 end
